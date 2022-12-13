ce960ff1fece784d9045d46052111d744ab65f72
HSkander <hamdiskander5@gmail.com>
2022-12-13 19:11:08 +0100

    First commit ui part done

---
+++ src/app/Device.ts
@@ -0,0 +1,7 @@
+export interface Device {
+    id: number;
+    devecename: string;
+    coordinates: string;
+    format: string;
+   
+  }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Device } from './Device';
+import { FormGroup , FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DeviceManager';
+  public addDeviceForm?: FormGroup;
+
+  public onAddDevice(/*addForm: NgForm*/): void {
+   
+  }
+  public onDeleteDevice(employeeId: number): void {
+    
+  }
+
+  /*public onOpenModal(device : Device , mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addDeviceModal');
+    }
+    
+    if (mode === 'delete') {
+     // this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }*/
+
 }
